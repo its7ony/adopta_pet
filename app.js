8ac82542558db6f810f5ab5054343b08dcd85cd2
Antonio Labra <antonio.labra@forteinnovation.mx>
2021-09-22 20:39:51 -0500

    subir codigo para hacer deploy

---
+++ app.js
@@ -0,0 +1,31 @@
+// Express
+const express = require('express');
+const app = express();
+
+//Body Parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Configuración de base de datos
+const mongoose = require('mongoose');
+
+
+mongoose.connect(process.env.MONGO_URI);
+
+mongoose.set("debug", true)
+
+require('./models/Usuario')
+require('./models/Mascota')
+
+require('./config/passport')
+
+//Rutas
+app.use('/v1', require('./routes'));
+
+// Iniciando el servidor
+
+app.listen(process.env.PORT, () => {
+	console.log(`Server listening on port ${process.env.PORT}`)
+})
+
